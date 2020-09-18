5e1bd59b0d99117fdd646327dd7594226916f591
gultekinkaan <65185866+gultekinkaan@users.noreply.github.com>
2020-09-18 20:54:18 +0300

    Update save.py

--- save.py
+++ save.py
@@ -8,14 +8,6 @@ while True:
     out = ("%d-%d-%d%-d-%d-%d" % (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
     time.sleep(DELAY)
 
-    stream_url = "http://scturkmedya.radyotvonline.com/stream/80/"
-    r = requests.get(stream_url, stream=True)
-    with open(out, 'wb') as f:
-        try:
-            for block in r.iter_content(1024):
-                f.write(block)
-        except KeyboardInterrupt:
-            pass
 
     files_path = "C:\\Users\\hp\\PycharmProjects\\orenıoz\\"
     file_name = out
