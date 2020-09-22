a1acd4af588ca367023e64528a054ef930314ff9
gultekinkaan <65185866+gultekinkaan@users.noreply.github.com>
2020-09-22 23:57:21 +0300

    Update save.py

--- save.py
+++ save.py
@@ -1,36 +1,40 @@
+import sys
 import time
 import requests
 from pydub import AudioSegment
-DELAY = 1 * 1 * 1
-t = time.localtime()
-out = ("%d-%d-%d%-d-%d-%d.mp3" % (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
-time.sleep(DELAY)
-
-stream_url = "http://scturkmedya.radyotvonline.com/stream/80/"
-r = requests.get(stream_url, stream=True)
-with open(out, 'wb') as f:
-    try:
-        for block in r.iter_content(1024):
-            f.write(block)
-    except KeyboardInterrupt:
-         pass
-
-files_path = "C:\\Users\\hp\\PycharmProjects\\orenıoz\\"
-file_name = out
-
-startMin = 1
-startSec = 00
-
-endMin = 2
-endSec = 00
-
-startTime = startMin * 1 * 1 + startSec * 00
-endTime = endMin * 2 * 1 + endSec * 00
+while True:
+    DELAY = 1 * 1 * 1
+    t = time.localtime()
+    out = ("%d-%d-%d%-d-%d-%d.mp3" % (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
+    time.sleep(DELAY)
+    sys.stdout.write(out)
+    sys.stdout.flush()
 
 
-song = AudioSegment.from_mp3(files_path + file_name + '.mp3')
-extract = song[startTime: endTime]
-extract.export(file_name + '-extract.mp3', format="mp3")
+    files_path = "C:\\Users\\hp\\PycharmProjects\\orenıoz\\"
+    file_name = out
+
+    startMin = 0
+    startSec = 00
+
+    endMin = 2
+    endSec = 10
+    startTime = startMin * 1 * 1 + startSec * 1
+    endTime = endMin * 1 * 1 + endSec * 1
+
+    stream_url = "http://scturkmedya.radyotvonline.com/stream/80/"
+    r = requests.get(stream_url, stream=True)
+    with open(out, 'wb') as f:
+        try:
+            for block in r.iter_content(1024):
+                f.write(block)
+        except KeyboardInterrupt:
+            pass
+
+    song = AudioSegment.from_mp3(files_path + file_name)
+    extract = song[startTime: endTime]
+    extract.export(file_name + '-extract.mp3', format="mp3")
+
 
 
 
