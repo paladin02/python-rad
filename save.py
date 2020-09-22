361bb10f871ae8d9ea1e2a75d96205debb87a16d
gultekinkaan <65185866+gultekinkaan@users.noreply.github.com>
2020-09-23 00:33:59 +0300

    Update save.py

--- save.py
+++ save.py
@@ -5,11 +5,9 @@ from pydub import AudioSegment
 while True:
     DELAY = 1 * 1 * 1
     t = time.localtime()
-    out = ("%d-%d-%d%-d-%d-%d.mp3" % (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
+    out = ("%d-%d-%d-%d-%d-%d.mp3" % (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
     time.sleep(DELAY)
-    sys.stdout.write(out)
-    sys.stdout.flush()
-
+    print(out)
 
     files_path = "C:\\Users\\hp\\PycharmProjects\\orenıoz\\"
     file_name = out
@@ -17,8 +15,8 @@ while True:
     startMin = 0
     startSec = 00
 
-    endMin = 2
-    endSec = 10
+    endMin = 0
+    endSec = 8
     startTime = startMin * 1 * 1 + startSec * 1
     endTime = endMin * 1 * 1 + endSec * 1
 
@@ -34,11 +32,3 @@ while True:
     song = AudioSegment.from_mp3(files_path + file_name)
     extract = song[startTime: endTime]
     extract.export(file_name + '-extract.mp3', format="mp3")
-
-
-
-
-
-
-
-
