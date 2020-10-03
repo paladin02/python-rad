59c223aef174cea6bf61d4ccfd317a15a735149d
gultekinkaan <65185866+gultekinkaan@users.noreply.github.com>
2020-10-03 12:20:22 +0300

    Update save.py

--- save.py
+++ save.py
@@ -1,24 +1,12 @@
-import sys
 import time
 import requests
-from pydub import AudioSegment
 while True:
-    DELAY = 1 * 1 * 1
     t = time.localtime()
+
     out = ("%d-%d-%d-%d-%d-%d.mp3" % (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
-    time.sleep(DELAY)
+
+    time.sleep(1)
     print(out)
-
-    files_path = "C:\\Users\\hp\\PycharmProjects\\orenıoz\\"
-    file_name = out
-
-    startMin = 0
-    startSec = 00
-
-    endMin = 0
-    endSec = 8
-    startTime = startMin * 1 * 1 + startSec * 1
-    endTime = endMin * 1 * 1 + endSec * 1
 
     stream_url = "http://scturkmedya.radyotvonline.com/stream/80/"
     r = requests.get(stream_url, stream=True)
@@ -28,7 +16,3 @@ while True:
                 f.write(block)
         except KeyboardInterrupt:
             pass
-
-    song = AudioSegment.from_mp3(files_path + file_name)
-    extract = song[startTime: endTime]
-    extract.export(file_name + '-extract.mp3', format="mp3")
