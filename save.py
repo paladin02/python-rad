1aa3ce98438024c03bbd3e5ca185393968facfa9
gultekinkaan <65185866+gultekinkaan@users.noreply.github.com>
2020-10-05 22:29:42 +0300

    Update save.py

--- save.py
+++ save.py
@@ -1,22 +1,18 @@
 import time
 import requests
-
 while True:
     t = time.localtime()
 
     out = ("%d-%d-%d-%d-%d-%d.mp3" % (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
 
     print(out)
-    time.sleep(5)
+    time.sleep(2)
 
     stream_url = "http://scturkmedya.radyotvonline.com/stream/80/"
     r = requests.get(stream_url, stream=True)
 
     with open(out, 'wb') as f:
-        try:
-            for block in r.iter_content(1024):
-                f.write(block)
-        except KeyboardInterrupt:
-            pass
-        finally:
-            f.close()
+        for block in r.iter_content(1024):
+
+            f.write(block)
+            break
