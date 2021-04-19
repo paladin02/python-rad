07cb42b6dbf75f0f6b83c9f16eabf23849151535
paladin02 <65185866+paladin02@users.noreply.github.com>
2021-04-20 01:49:01 +0300

    Create save2.py

---
+++ save2.py
@@ -0,0 +1,16 @@
+import time
+import requests
+import os
+import sys
+while True:
+    t = time.localtime()
+    out = ("%d-%d-%d-%d-%d-%d.mp3" % (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
+    print(out)
+    time.sleep(3)
+    stream_url = "http://scturkmedya.radyotvonline.com/stream/80/"
+    r = requests.get(stream_url, stream=True)
+    with open(out,"wb") as f:
+        for block in r.iter_content(1024):
+             f.write(block)
+             time.sleep(3)
+             os.execl(sys.executable, sys.executable, *sys.argv)
