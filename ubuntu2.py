19b305c9f69f4f9b445b54f32f04e07f3ef5a832
paladin02 <65185866+paladin02@users.noreply.github.com>
2021-04-26 20:46:18 +0300

    Create ubuntu2.py

---
+++ ubuntu2.py
@@ -0,0 +1,42 @@
+import os
+import time
+import requests
+import sys
+import psycopg2
+try:
+	conn = psycopg2.connect("dbname='hasbel' user='paladin' password='123'")
+	print("Connected")
+except:
+	print("No")	
+try:
+	conn1 = psycopg2.connect("dbname='deneme' user='paladin' password='123'");
+	print("Connected")
+except:
+	print("no")
+cursor = conn.cursor()
+cursor2 = conn1.cursor()
+try:
+	cursor.execute("SELECT url FROM admin")
+	rows = cursor.fetchall()
+	for rows in rows:
+		link = rows[0]
+		print(link)
+except:
+	print("no")
+
+path = "/home/paladin/Masaüstü/"
+try:
+    os.chdir(path)
+    print("Current working directory: {0}".format(os.getcwd()))
+except FileNotFoundError:
+    pass
+while True:
+    t = time.localtime()
+    out = ("%d-%d-%d-%d-%d-%d.mp3" % (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
+    print(out)
+    time.sleep(3)
+    r = requests.get(link, stream=True)
+    with open(out,"wb") as f:
+        for block in r.iter_content(1024):
+            f.write(block)
+            break
